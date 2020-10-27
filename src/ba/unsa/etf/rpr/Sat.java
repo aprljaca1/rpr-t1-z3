a5501f39de1e09d142975320b9dce63ee43f7a10
aprljaca1 <aprljaca1@etf.unsa.ba>
2020-10-27 17:05:49 +0100

    Prepravljena klasa Sat

--- src/ba/unsa/etf/rpr/Sat.java
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -1,29 +1,46 @@
 package ba.unsa.etf.rpr;
 
-class Sat {
-    int sati,minute,sekunde;
-    public:
-    Sat(int sati, int minute, int sekunde) { Postavi(sati,minute,sekunde); }
-    void Postavi(int sati, int minute, int sekunde) { Sat::sati=sati; Sat::minute=minute; Sat::sekunde=sekunde; }
-    void Sljedeci() {
+public class Sat {
+    private int sati,minute,sekunde;
+
+    Sat(int sati, int minute, int sekunde) {
+        Postavi(sati,minute,sekunde); }
+
+    void Postavi(int sati, int minute, int sekunde) {
+        this.sati=sati; this.minute=minute; this.sekunde=sekunde; }
+
+    public void Sljedeci() {
         sekunde++;
         if (sekunde==60) { sekunde=0; minute++; }
         if (minute==60) { minute=0; sati++; }
         if (sati==24) sati=0;
     }
-    void Prethodni() {
+    public void Prethodni() {
         sekunde--;
         if (sekunde==-1) { sekunde=59; minute--; }
         if (minute==-1) { minute=59; sati--; }
         if (sati==-1) sati=23;
     }
     void PomjeriZa(int pomak) {
-        if (pomak>0) for (int i(0); i<pomak; i++) Sljedeci();
-		else for (int i(0); i<-pomak; i++) Prethodni();
+        if (pomak>0) for (int i=0; i<pomak; i++) Sljedeci();
+		else for (int i=0; i<-pomak; i++)
+		    Prethodni();
     }
-    int DajSate() const { return sati; }
-    int DajMinute() const { return minute; }
-    int DajSekunde() const { return sekunde; }
-    void Ispisi() const { cout<<sati<<":"<<minute<<":"<<sekunde<<endl; }
+
+    public void Ispisi() {
+        System.out.println("Sati: "+ sati + " minute: "+ minute + " sekunde: "+ sekunde);
+    }
+
+    public int getSati() {
+        return sati;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSekunde() {
+        return sekunde;
+    }
 }
 
